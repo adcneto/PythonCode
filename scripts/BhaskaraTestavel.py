d7cfd58b6afce6f0c6ecf29423c8dad420bb1d7f
adcneto <39680902+adcneto@users.noreply.github.com>
2018-05-27 20:49:04 -0300

    Update BhaskaraTestavel.py

--- scripts/BhaskaraTestavel.py
+++ scripts/BhaskaraTestavel.py
@@ -6,7 +6,7 @@ class Bhaskara:
 # Não foi necessario utlizar o metodo construtor __init__
 
     def main(self):
-        Adigitado = float(input("Digite o valor de a: "))
+        Adigitado = float(input("Digite o valor do numero a: "))
         Bdigitado = float(input("Digite o valor de b: "))
         Cdigitado = float(input("Digite o valor de c: "))
         print(self.CalculaRaizes2Grau(Adigitado, Bdigitado, Cdigitado))
